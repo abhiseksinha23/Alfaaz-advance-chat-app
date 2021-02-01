8e03dd503df77a2ed3ba43a112733e5b9121da64
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2021-02-01 21:29:46 +0530

    Aflaaz - realtime chat app

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+//adding Socket-io to the project
+const socket = require('socket.io');
+
+ //creating server for running node app and socket-io
+const server = app.listen(8000,()=>{
+  console.log("server connected at 8000");
+});
+
+app.use(express.static('public'));
+
+//Connecting to the server - Node server which will handle socket io connection
+const io = socket(server);
+
+//Default route
+app.get("/", (req,res)=>{
+  res.render("index.html");
+});
+
+//Store users as they join and leave
+const users={}
+
+//Establish the connection from backend(Server) for the frontend
+io.on('connection', (socket)=>{
+  //console.log("connected ");
+
+  //New user joined, collect user's name and store it corresponding to socket.id assigned to the user in users object
+  socket.on('new-user-joined', name=>{
+    users[socket.id] = name;
+    //console.log("New user", name);
+    socket.broadcast.emit('user-joined', name); //broadcast everyone else about the new joined user
+  });
+
+  //Send message to everyone except the sender (doing it in frontend itself for sender)
+  socket.on('send', message=>{
+    socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+  });
+
+	// Handle typing event
+    socket.on('typing', (name) => {
+        socket.broadcast.emit('typing', name);
+    });
+
+    // Handle disconnect event, whenever someone leaves the chat notify others
+    //disconnect is a keyword and it can not be replaced by any other name as it is fired by server itself
+    socket.on('disconnect',message=>{
+      socket.broadcast.emit('left',users[socket.id]);
+      delete users[socket.id];
+    });
+});
---
+++ public/client.js
@@ -0,0 +1,69 @@
+//Ask the new user, his/her name
+const name = prompt("Enter your name to join");
+
+//Establish connections between frontend and backend using the CDN in the main html file
+//Do it after the promt alert activities
+const socket = io.connect("http://localhost:8000/"); //connected it to the backend
+
+//Collecting all the elements needed to display and receive messages
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+const feedback = document.getElementById('feedback');
+var audio = new Audio('ding.mp3'); //audio for the messages
+
+
+//Function to add the messages to the container
+const append = (message,position,sound) =>{
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageElement.classList.add('message');
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement); //adding the new message to the container
+  if(sound == 1){
+     audio.play(); //play message
+  }
+}
+
+//take the input message and send to it to all the other recivers and add it to the frontend for all
+// add the message to the senders screen as well (cause we are broadcasting the message not emiting it)
+form.addEventListener('submit', (e)=>{
+  e.preventDefault(); //page doesn't reloads
+  const message = messageInp.value;
+  append(`You: ${message}`,'right',0);
+  socket.emit('send',message);
+  messageInp.value = '';
+});
+
+//Listen to keypress element and notify other that someone is typing
+messageInp.addEventListener('keypress', function(){
+  //  console('typing');
+    socket.emit('typing',name);
+})
+
+//whenever someone joins, call the new-user-joined with passing the user's name
+socket.emit('new-user-joined', name);
+
+//Notify all about the new user by adding a message to the char
+socket.on('user-joined', name =>{
+  append(`${name} joined the chat`, 'right',1);
+});
+
+//Send message to the screen of all the recivers
+socket.on('receive', data =>{
+  append(`${data.name}: ${data.message} `, 'left',1);
+   feedback.innerHTML='<p><em>Status...</em></p>';
+});
+
+//Adding the name of the person who's currently typing
+socket.on('typing', name => {
+    feedback.innerHTML = '<p><em>' + name + ' is typing a message...</em></p>';
+  //  appends(`${p}`,'left');
+});
+
+//Notify others who left the chat
+socket.on('left', name =>{
+  append(`${name} has left the chat`,'right',1);
+});
+
+//things left - image, scroll below
